ea32869ac30f902cab1b216c06c9d65fba45e453
Clark Montano <clrk.montano@gmail.com>
2023-04-09 22:42:19 +0800

    Initial commit

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ with open("./Input/Names/invited_names.txt") as name:
     for line in name.readlines():
         name_list.append(line.strip())
 
-for names in name_list:
-    with open("./Input/Letters/starting_letter.txt") as text:
-        main_text = text.read()
-    main_text = main_text.replace("[name]", names)
-    with open(f"Output/ReadyToSend/Letter_for_{names}.txt", "w") as letter:
-        letter.write(main_text)
+
+with open("./Input/Letters/starting_letter.txt") as text:
+    main_text = text.read()
+    for names in name_list:
+        read_text = main_text.replace("[name]", names)
+        with open(f"Output/ReadyToSend/Letter_for_{names}.txt", "w") as letter:
+            letter.write(read_text)
