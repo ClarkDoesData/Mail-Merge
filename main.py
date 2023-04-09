a85ff4877273642742629ed4c00d7f5edc8fda42
Clark Montano <clrk.montano@gmail.com>
2023-04-09 22:46:10 +0800

    Initial commit

--- main.py
+++ main.py
@@ -3,14 +3,12 @@
 # Replace the [name] placeholder with the actual name.
 # Save the letters in the folder "ReadyToSend".
 with open("./Input/Names/invited_names.txt") as name:
-    name_list = []
-    for line in name.readlines():
-        name_list.append(line.strip())
-
+    name_list = name.readlines()
 
 with open("./Input/Letters/starting_letter.txt") as text:
     main_text = text.read()
     for names in name_list:
-        read_text = main_text.replace("[name]", names)
-        with open(f"Output/ReadyToSend/Letter_for_{names}.txt", "w") as letter:
+        stripped_name = names.strip()
+        read_text = main_text.replace("[name]", stripped_name)
+        with open(f"Output/ReadyToSend/Letter_for_{stripped_name}.txt", "w") as letter:
             letter.write(read_text)
