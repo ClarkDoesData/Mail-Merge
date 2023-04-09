248544b2ffbbc61abdea4af0c1efaea7aa2d10c5
Clark Montano <clrk.montano@gmail.com>
2023-04-09 20:55:09 +0800

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+with open("./Input/Names/invited_names.txt") as name:
+    name_list = []
+    for line in name.readlines():
+        name_list.append(line.strip())
+
+for names in name_list:
+    with open("./Input/Letters/starting_letter.txt") as text:
+        main_text = text.read()
+    main_text = main_text.replace("[name]", names)
+    #print(main_text)
+    with open(f"Output/ReadyToSend/Letter_for_{names}.txt", "w") as letter:
+        letter.write(main_text)
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
